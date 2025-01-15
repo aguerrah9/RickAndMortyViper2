7bfb6abdd873cad9d866100d9f4623be3c10f68e
1140074 <alejandro.guerrah@elektra.com.mx>
2025-01-15 17:56:40 -0600

    Initial Commit

---
+++ RickAndMortyViper2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyViper2
+//
+//  Created by Alejandro Guerra Hernandez on 15/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
